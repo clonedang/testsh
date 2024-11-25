743f27aa77c033d630349327ef5d64c2cab198c0
clonedang <dangtop1fsure@gmail.com>
2024-11-25 19:06:12 +0700

    Update rtdetr.py

--- src/train/rtdetr.py
+++ src/train/rtdetr.py
@@ -2,11 +2,11 @@ import wandb
 from ultralytics import RTDETR
 import argparse
 
-def train_rtdetr(weight_path, num_epochs, wandb_key, data_yaml):
+def train_rtdetr(weight_path, num_epochs, wandb_key, data_yaml, dev):
     wandb.login(key=wandb_key)
 
     model = RTDETR(weight_path)
-    results = model.train(data=data_yaml, imgsz=1280, batch=4, close_mosaic=15, epochs=num_epochs)
+    results = model.train(data=data_yaml, imgsz=1280, batch=4, close_mosaic=15, epochs=num_epochs, device=dev)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Train RTDETR")
@@ -30,6 +30,11 @@ if __name__ == "__main__":
         type=str,
         default="rtdetr-x.pt"
     )
+    parser.add_argument(
+        "-d",
+        "--device",
+        type=int,
+    )
 
     args = parser.parse_args()
-    train_rtdetr(args.weight_path, args.epochs, args.wandb_key, args.yaml)
+    train_rtdetr(args.weight_path, args.epochs, args.wandb_key, args.yaml, args.device)
