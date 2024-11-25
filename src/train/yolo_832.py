406d416cb6e7147367273f95db979b61ef0b7124
clonedang <dangtop1fsure@gmail.com>
2024-11-25 20:46:37 +0700

    Update yolo_832.py

--- src/train/yolo_832.py
+++ src/train/yolo_832.py
@@ -6,7 +6,7 @@ def train_yolov11_832(weight_path, num_epochs, wandb_key, data_yaml, dev):
     wandb.login(key=wandb_key)
 
     model = YOLO(weight_path)
-    results = model.train(data=data_yaml, imgsz=832, batch=12, close_mosaic=15, epochs=num_epochs, device=dev)
+    results = model.train(data=data_yaml, imgsz=832, batch=8, close_mosaic=15, epochs=num_epochs, device=dev)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Train yolov11 default")
